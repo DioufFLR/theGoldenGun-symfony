0585611fe568320911207b1ffc0a920df743353c
diouf <geoffreyfleur@gmail.com>
2023-12-21 11:56:00 +0100

    :ukraine: Implement user listing in admin panel
    UserRepository has been integrated into the Admin UserController to handle user data. The user data retrieved is then displayed on the admin user page. The user interface is updated on the templates/admin/user/index.html.twig, presenting the user data in a table, each row representing a single user.

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     #[Route('/admin/users', name: 'admin_users_')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
         return $this->render('admin/user/index.html.twig', [
             'controller_name' => 'Gestion des Utilisateurs',
+            'users' => $users
         ]);
     }
 }
